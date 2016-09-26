f23bd576886bf7d3e071bd20acdf7ae700a4b6a9
Martin Homuth <martin@martinhomuth.de>
2016-09-26 08:24:16 +0200

    Some work on linked_list
    Signed-off-by: Martin Homuth <martin@martinhomuth.de>

--- ex1a/linked_list.h
+++ ex1a/linked_list.h
@@ -9,8 +9,8 @@ struct linked_list {
 struct linked_list *head;
 struct linked_list *z;
 
-void initialize_list(struct linked_list *);
+void initialize_list(struct linked_list **);
 struct linked_list *delete(struct linked_list*);
-void append(struct linked_list*);
+void append_to_list(struct linked_list*);
 
 #endif
